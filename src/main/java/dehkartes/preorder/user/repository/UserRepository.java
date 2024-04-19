7677be57f8138e31a89f4496aeaaf51593cfe78a
Dehkartes <hsj3925@naver.com>
2024-04-19 16:11:17 +0900

    clean

--- src/main/java/dehkartes/preorder/user/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dehkartes.preorder.user.repository;
-
-import dehkartes.preorder.user.dto.UserDTO;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<UserDTO, String> {
-}
